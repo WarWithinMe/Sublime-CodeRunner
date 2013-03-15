a043bc9f9e93dcd7a3b464d27ecf925c3b723428
WarWithinMe <liangmorr@gmail.com>
2013-03-15 15:08:16 +0800

    Port to ST3

--- coderunner.py
+++ coderunner.py
@@ -9,118 +9,120 @@ import tempfile
 
 class ThreadProgress():
     """
-    Class taken from Package Control
-
-    :param thread:
+        Class taken from Package Control
+        
+        :param thread:
         The thread to track for activity
-
-    :param message:
+        
+        :param message:
         The message to display next to the activity indicator
-    """
-
+        """
+    
     def __init__(self, thread, message):
         self.thread  = thread
         self.message = message
         self.addend  = 1
         self.size    = 8
         sublime.set_timeout(lambda: self.run(0), 100)
-
+    
     def run(self, i):
         if not self.thread.is_alive():
             sublime.status_message('' if hasattr(self.thread, 'result') and not self.thread.result else self.thread.result)
             return
-
+        
         before = i % self.size
         after = (self.size - 1) - before
-
+        
         sublime.status_message('%s [%s=%s]' % \
-            (self.message, ' ' * before, ' ' * after))
-
+                               (self.message, ' ' * before, ' ' * after))
+        
         if not after:
             self.addend = -1
         if not before:
             self.addend = 1
         i += self.addend
-
+        
         sublime.set_timeout(lambda: self.run(i), 100)
 
 
 class RunThread(threading.Thread):
-
+    
     command = ""
     result  = ""
-
+    
     def __init__(self, command, path, args, commandObj):
-
+        
         self.command    = command
         self.commandObj = commandObj
         self.path       = '"' + path + '"'
         self.args       = args
         threading.Thread.__init__(self)
-        
+    
     def run(self):
         # Some program may be in /usr/local/bin
         env = os.environ.copy()
         env["PATH"] += ":/usr/local/bin"
-
+        
         # And count the eclapsed time
-
+        
         eclapsed = time.time()
-
+        
         cmd = self.command.replace("{{file}}", self.path).replace("{{args}}", self.args)
-
+        
         run = subprocess.Popen(cmd
-                , bufsize = -1
-                , shell   = True
-                , stdout  = subprocess.PIPE
-                , stderr  = subprocess.PIPE
-                , env     = env)
-
+                               , bufsize = -1
+                               , shell   = True
+                               , stdout  = subprocess.PIPE
+                               , stderr  = subprocess.PIPE
+                               , env     = env)
+        
         stdout, stderr = run.communicate()
-
+        
         eclapsed = int( (time.time() - eclapsed) * 1000 )
-
+        
         if stderr:
             self.result = "Run Failed"
+            sys.stdout.write(str(stderr))
         else:
             self.result = "Run Completed in {0}ms".format(eclapsed)
-
-        sys.stdout.write(stderr)
-
+        
+        
+        
         # Collect stdout and stderr, show them in a new buffer
         if stderr or stdout:
-            self.stdout = stdout
-            self.stderr = stderr
+            self.stdout = stdout.decode(sys.getfilesystemencoding())
+            self.stderr = stderr.decode(sys.getfilesystemencoding())
             sublime.set_timeout(lambda : self.show_res(), 10)
-
+    
     def show_res(self):
         
         template = """/*
- * Run Result from : {0}
- */
-
-{1}
-"""
+            * Run Result from : {0}
+            */
+            
+            {1}
+            """
         command_settings = sublime.load_settings("coderunner.sublime-settings")
-
+        
         if command_settings.get( "show_result_in_buffer" ):
             new_view = self.commandObj.window.get_output_panel("run_result")
             self.commandObj.window.run_command("show_panel", {"panel" : "output.run_result" })
         else:
             new_view = self.commandObj.window.new_file()
-
+        
         new_view.set_name("Run Result")
         new_view.set_read_only(False)
-
-        edit   = new_view.begin_edit()
-        output = template.format( self.path, self.stdout or self.stderr )
-
-        new_view.insert( edit, 0, output.decode(sys.getfilesystemencoding()) )
-        new_view.end_edit(edit)
+        
+        output = template.format( self.path, (self.stdout or self.stderr) )
+        new_view.run_command("run_code_show_result", { "args" : output })
         
         new_view.set_read_only(True)
         new_view.set_scratch(True)
         new_view.settings().set( 'syntax', 'Packages/JavaScript/JavaScript.tmLanguage' )
+
+class RunCodeShowResultCommand(sublime_plugin.TextCommand):
+    def run(self, edit, args):
+        self.view.insert( edit, 0, args )
 
 
 class RunCodeCommand(sublime_plugin.WindowCommand):
@@ -128,20 +130,19 @@ class RunCodeCommand(sublime_plugin.WindowCommand):
         view     = self.window.active_view()
         settings = view.settings()
         syntax   = settings.get( 'syntax' )
-
+        
         language = os.path.basename(syntax).replace('.tmLanguage', '').lower() if syntax != None else "plain text"
-
+        
         command_settings = sublime.load_settings("coderunner.sublime-settings")
         commands         = command_settings.get("commands")
         if not language in commands:
-          return
-
+            return
+        
         # Generate a thread to execute the command
-        sys.stdout.write(tempfile.tempdir)
         filename = view.file_name()
         if view.is_dirty():
             # Save the dirty content to a temp file
-            filename = os.path.normpath(tempfile.tempdir) + "/" + command_settings.get("temp_file_name")
+            filename = os.path.normpath(tempfile.gettempdir()) + "/" + command_settings.get("temp_file_name")
             try:
                 temp = open(filename, "w")
             except:
@@ -149,23 +150,23 @@ class RunCodeCommand(sublime_plugin.WindowCommand):
                 return
             temp.write(view.substr(sublime.Region(0,view.size())))
             temp.close()
-
+        
         thread = RunThread( commands[language], filename, args , self )
         thread.start()
-
+        
         # Show process indicator
         ThreadProgress(thread, "Running...")
 
 
 class RunCodeCustomCommand(sublime_plugin.WindowCommand):
-
+    
     lastArgs = ""
-
+    
     def run(self):
         self.window.show_input_panel("Custom Arguments"
-            , self.lastArgs
-            , lambda a : self.window.run_command("run_code", { "args" : a } )
-            , None
-            , None)
+                                     , self.lastArgs
+                                     , lambda a : self.window.run_command("run_code", { "args" : a } )
+                                     , None
+                                     , None)
 
-    
+
